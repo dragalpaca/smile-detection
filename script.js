62bdf36780c5a9ccf625c5d027929dafcdef2176
dragalpaca <44612263+dragalpaca@users.noreply.github.com>
2019-06-04 18:11:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('dragalpaca.github.io/smile-detection/models'),
+  faceapi.nets.tinyFaceDetector.loadFromUri('../models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
   faceapi.nets.faceExpressionNet.loadFromUri('../models')
