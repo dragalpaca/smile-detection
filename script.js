8aa17389617874c51f0d4908562899d5d4633a52
anna <>
2019-06-04 17:35:09 +0200

    change path to old

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('smile-detection/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('smile-detection/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('smile-detection/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('smile-detection/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
 
 function startVideo() {
